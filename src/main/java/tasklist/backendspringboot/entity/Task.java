548bc44f45c89b7de5dabb7c082ae92bf93c405c
Artem <footerka2@gmail.com>
2021-08-16 22:21:50 +0300

    Create entity

---
+++ src/main/java/tasklist/backendspringboot/entity/Task.java
@@ -0,0 +1,63 @@
+package tasklist.backendspringboot.entity;
+
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+
+@Entity
+@NoArgsConstructor
+@EqualsAndHashCode
+@Setter
+public class Task {
+    private Long id;
+    private String title;
+    private Integer completed;
+    private Date date;
+    private Priority priority;
+    private Category categoryByCategoryId;
+
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id")
+    public Long getId() {
+        return id;
+    }
+
+    @Basic
+    @Column(name = "title")
+    public String getTitle() {
+        return title;
+    }
+
+    @Basic
+    @Column(name = "completed")
+    public Integer getCompleted() {
+        return completed;
+    }
+
+    @Basic
+    @Column(name = "date")
+    public Date getDate() {
+        return date;
+    }
+
+
+    @ManyToOne
+    @JoinColumn(name = "priority_id", referencedColumnName = "id")
+    public Priority getPriority() {
+        return priority;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", referencedColumnName = "id")
+    public Category getCategoryByCategoryId() {
+        return categoryByCategoryId;
+    }
+
+
+}
